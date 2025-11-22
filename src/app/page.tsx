4b36a56a8dbd324fab43607e9662d58bfed02d0a
Artkurkur <2002artnomer.24@gmail.com>
2025-11-22 16:08:39 +0800

    hays

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+// src/app/page.tsx
+import type { Metadata } from "next";
+import Link from "next/link";
+
+// Optional: Metadata for SEO / tab title
+export const metadata: Metadata = {
+  title: "URDS MAIN TEMPLATE",
+  description: "This is URDS LANDING PAGE",
+};
+
+// src/app/page.tsx
+import { redirect } from "next/navigation";
+
+export default function Home() {
+  // Redirect root "/" to URDS folder
+  redirect("/signin");
+}
