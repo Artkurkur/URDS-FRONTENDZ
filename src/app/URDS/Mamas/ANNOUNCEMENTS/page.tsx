2e082b5c3afc1bcc009aa84366e2a37fc32c124c
Abiathar Corsino <abiatharcorsino@gmail.com>
2025-12-03 17:38:22 +0800

    mamas-update

--- src/app/URDS/Mamas/ANNOUNCEMENTS/page.tsx
+++ src/app/URDS/Mamas/ANNOUNCEMENTS/page.tsx
@@ -46,7 +46,7 @@ const AnnouncementInterface = () => {
     { value: 'year', label: 'This Year' },
     { value: '2024', label: '2024' },
     { value: '2023', label: '2023' },
-    { value: '2022', label: '2023' }
+    { value: '2022', label: '2022' }
   ];
 
   const filterAnnouncements = () => {
